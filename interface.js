aedcb457f81e5b7ed2d6d6424ceea9918df4bdaa
Michela <m.deodati@studenti.unipi.it>
2023-09-15 11:58:36 +0200

    ad way to calculate the date to obtain the past period measurement

--- interface.js
+++ interface.js
@@ -10,6 +10,7 @@ require('dotenv').config({
 
 const {Client,Pool} = require ('pg');
 
+//save table name 
 const kpi_table_name = process.env.KPI_TABLE_NAME;
 const data_table_name = process.env.DATA_TABLE_NAME;
 
@@ -26,8 +27,10 @@ const pool = new Pool({
 
     const paramsObj = {
         timeSecond: 70300,
-        startDate: "'2023-05-01'",
-        endDate: "'2023-06-01'",
+        startDatePast: dateCalculator('2023-06-01',['2023-06-01','2023-07-01']),
+        endDatePast:   "'2023-06-01'",
+        startDate: "'2023-06-01'",
+        endDate: "'2023-07-01'",
         dataTableName: data_table_name
     };
     
@@ -35,7 +38,7 @@ const pool = new Pool({
     const client = await pool.connect();
     // console.log("client on");
     try{  
-        const kpiId = toParams("power_cost_per_hour") ;
+        const kpiId = toParams("machine_usage_trend") ;
         const kpiRows = await client.query(kpiColumQuery(kpiId, kpi_table_name));
         // console.log(kpiColumQuery(kpiId, kpi_table_name));
          //console.log(kpiRows);
@@ -57,7 +60,7 @@ const pool = new Pool({
                 //console.log(replaceWithValue(childRetRow.rows[0]['query'], paramsObj));
                 var value ={
                     id: child,
-                    valueRow : (await client.query(replaceWithValue(childRetRow.rows[0]['query'], paramsObj))).rows[0]['v']
+                    value: (await client.query(replaceWithValue(childRetRow.rows[0]['query'], paramsObj))).rows[0]['v']
                 };
                 childArray.push(value);
             }
@@ -106,7 +109,35 @@ function kpiColumQuery (kpi_id, table_name){
     return ('select id,query,children,function from '+table_name +" where id ="+ kpi_id);
 }
 
-//convert hours into seconds, the time unit measured from Iot device
-function hoursToSecond(hours){
-    return hours*60*60;
+//set the date to past 5 cycle period measuring difference with previuous period
+function dateCalculator(date, [strD, endD]){
+    const s = new Date(strD);
+    const e = new Date(endD);
+    //day subtraction is measured by milliseconds
+    const period = Math.ceil(Math.abs(e-s)/ (1000*60*60*24)); 
+    
+    var pastDate = new Date (date);
+
+    if(period >= 10){
+        //user selected period is more than 10 days, period cycle past report is month
+        //console.log("More than 10 days");
+        //past 5 period evaluation 
+        pastDate.setMonth(pastDate.getMonth() - 5);
+    }else{
+        //user selected period is less then 10 days, period cycle past report is week
+        //console.log("Less than 10 days");
+        pastDate.setDate(pastDate.getDate() - 35);
+    }
+    
+    var yyyy = pastDate.getFullYear(); // Get the 4-digit year
+    var mm = (pastDate.getMonth() + 1).toString().padStart(2, '0'); // Get the month (1-12) and format with a leading zero if necessary
+    var dd = pastDate.getDate().toString().padStart(2, '0'); // Get the day of the month and format with a leading zero if necessary
+    
+    var formattedDate = `${yyyy}-${mm}-${dd}`; // Create the formatted date string
+
+    //console.log("Input Date: "+date+" Output Date: "+toParams(formattedDate));
+    return toParams(formattedDate);
+
 }
+
+
